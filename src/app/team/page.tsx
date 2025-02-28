1e88fd10b492e53c75a862dc71c2231804bc8a62
alucic2 <alucic2@illinois.edu>
2025-02-28 10:29:21 -0600

    made changes to Team page

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import PageSkeleton from "@/components/PageSkeleton";
 import SubHeading from "@/components/SubHeading";
-import { teamData } from "@/modules/TeamData";
+import { teamData } from "@/modules/teamData.2";
 import { motion, useInView } from "framer-motion";
 import { useRef } from "react";
 
--- src/modules/TeamData.ts
+++ src/modules/TeamData.ts
@@ -1,126 +1,110 @@
-export const teamData = [
-  {
-    id: 1,
-    title: "Principal Researchers",
-    members: [
-      {
-        name: "Ana Lucic",
-        image: `/peritext-website/images/ana.jpeg`,
-        description: [
-          `Dr. Ana Lučić is a Staff Research Scientist at the Illinois Applied Research Institute (ARI) at the University of Illinois at Urbana-Champaign. At ARI, Dr. Lučić leverages her expertise in natural language processing, literature-based discovery, data engineering, and data management. Her career has focused on deriving new knowledge from historical datasets and advancing computational methods in digital humanities. In 2017, she was awarded an Advanced Collaborative Support grant from the HathiTrust Research Center to support the "Reading Chicago Reading" project, an initiative examining Chicago’s reading habits and cultural heritage through computational analysis. Her work continues to bridge the gap between digital archives and meaningful, large-scale humanities research.`,
-          "Dr. Lučić holds a PhD from the School of Information Sciences at the University of Illinois, where her dissertation focused on extracting comparative facets from biomedical scholarly articles. Before joining ARI, she served as a Digital Scholarship Librarian at DePaul University and as a Data Manager and Analyst with the IKIDS research group at the Beckman Institute. Her diverse experience in academia and research equips her with a unique perspective on the challenges and opportunities in digital humanities and computational text analysis.",
-        ],
-        social:
-          "https://appliedresearch.illinois.edu/directory/profile/alucic2",
-      },
-      {
-        name: "John Shanahan",
-        image: "/peritext-website/images/john.jpeg",
-        description: [
-          "Dr. John Shanahan is an Associate Dean and the Director of Liberal Studies in the College of Liberal Arts and Social Sciences at DePaul University. He holds a PhD in English from Rutgers University and a B.Phil. in English and Philosophy from the University of Pittsburgh. Dr. Shanahan’s research spans seventeenth- and eighteenth-century English literature, the history of science and technology, science fiction, and digital humanities. He is the co-Principal Investigator on DePaul’s Reading Chicago Reading project, a digital humanities initiative supported by Lyrasis and the National Endowment for the Humanities that examines reading patterns and literary engagement in Chicago. This project embodies his dedication to interdisciplinary research, blending literary studies with computational methods to explore how communities interact with literature.",
-          "In addition to his research, Dr. Shanahan has served in several academic leadership roles, including Director of Undergraduate Studies in English and Director of the Graduate Program in English. Currently, he also leads DePaul’s Certificate Program in Digital Humanities, reflecting his commitment to integrating technology into the humanities curriculum.",
-        ],
-        social:
-          "https://las.depaul.edu/academics/english/faculty/Pages/john-shanahan.aspx",
-      },
-      {
-<<<<<<< HEAD
-      name:"Robin Burke".
-      image: "/peritext-website/images/robin.jpeg",
-      descriiption: [
-      "Professor Robin Burke conducts research in personalized recommender systems, a field he helped found and develop. His most recent projects explore fairness, accountability and transparency in recommendation through the integration of objectives from diverse stakeholders."
-     "Dr Burke obtained his PhD in Computer Science from Northwestern University. In addition to the University of Colorado, he has held positions at the University of Chicago, the University of California-Irvine, California State University - Fullerton, DePaul University and University College Dublin.",
- ],
 
-      social: "https://www.colorado.edu/cmci/people/information-science/robin-burke",
-=======
-        name: "Robin Burke",
+export const teamData = [
+    {
+      id: 1,
+      title: "Principal Researchers",
+      members: [
+        {
+          name: "Ana Lucic",
+          image: "/peritext-website/images/ana.jpeg",
+          description: [
+            "Dr. Ana Lučić is a Staff Research Scientist at the Illinois Applied Research Institute (ARI) at the University of Illinois at Urbana-Champaign. At ARI, Dr. Lučić leverages her expertise in natural language processing, literature-based discovery, data engineering, and data management. Her career has focused on deriving new knowledge from historical datasets and advancing computational methods in digital humanities. In 2017, she was awarded an Advanced Collaborative Support grant from the HathiTrust Research Center to support the "Reading Chicago Reading" project, an initiative examining Chicago’s reading habits and cultural heritage through computational analysis. Her work continues to bridge the gap between digital archives and meaningful, large-scale humanities research.",
+            "Dr. Lučić holds a PhD from the School of Information Sciences at the University of Illinois, where her dissertation focused on extracting comparative facets from biomedical scholarly articles. Before joining ARI, she served as a Digital Scholarship Librarian at DePaul University and as a Data Manager and Analyst with the IKIDS research group at the Beckman Institute. Her diverse experience in academia and research equips her with a unique perspective on the challenges and opportunities in digital humanities and computational text analysis.",
+          ],
+          social:
+            "https://appliedresearch.illinois.edu/directory/profile/alucic2",
+        },
+        {
+          name: "John Shanahan",
+          image: "/peritext-website/images/john.jpeg",
+          description: [
+            "Dr. John Shanahan is an Associate Dean and the Director of Liberal Studies in the College of Liberal Arts and Social Sciences at DePaul University. He holds a PhD in English from Rutgers University and a B.Phil. in English and Philosophy from the University of Pittsburgh. Dr. Shanahan’s research spans seventeenth- and eighteenth-century English literature, the history of science and technology, science fiction, and digital humanities. He is the co-Principal Investigator on DePaul’s Reading Chicago Reading project, a digital humanities initiative supported by Lyrasis and the National Endowment for the Humanities that examines reading patterns and literary engagement in Chicago. This project embodies his dedication to interdisciplinary research, blending literary studies with computational methods to explore how communities interact with literature.",
+            "In addition to his research, Dr. Shanahan has served in several academic leadership roles, including Director of Undergraduate Studies in English and Director of the Graduate Program in English. Currently, he also leads DePaul’s Certificate Program in Digital Humanities, reflecting his commitment to integrating technology into the humanities curriculum.",
+          ],
+          social:
+            "https://las.depaul.edu/academics/english/faculty/Pages/john-shanahan.aspx",
+        },
+        {
+        name:"Robin Burke",
         image: "/peritext-website/images/robin.jpeg",
-        description: [
-          "Professor Robin Burke conducts research in personalized recommender systems, a field he helped found and develop. His most recent projects explore fairness, accountability and transparency in recommendation through the integration of objectives from diverse stakeholders.",
+        descriiption: [
+        "Professor Robin Burke conducts research in personalized recommender systems, a field he helped found and develop. His most recent projects explore fairness, accountability and transparency in recommendation through the integration of objectives from diverse stakeholders.",
+        "Dr Burke obtained his PhD in Computer Science from Northwestern University. In addition to the University of Colorado, he has held positions at the University of Chicago, the University of California-Irvine, California State University - Fullerton, DePaul University and University College Dublin.",
         ],
-        social:
-          "https://www.colorado.edu/cmci/people/information-science/robin-burke",
->>>>>>> 5faf88c2b545a3661a0ebc768b99db8bf1f5d657
-      },
-    ],
-  },
-  {
-    id: 2,
-    title: "Advisory Board members",
-    members: [
-      {
-        name: "Stephen Downie",
-        image: "/peritext-website/images/stephen.jpeg",
-        description: [
-          "Dr. J. Stephen Downie is associate dean for research and a professor at the School of Information Sciences, and the Illinois codirector of the HathiTrust Research Center. He has been an active participant in the digital libraries and digital humanities research domains.",
-        ],
-        social: "https://ischool.illinois.edu/people/j-stephen-downie",
-      },
-      {
-        name: "Glen Layne-Worthey",
-        image: `/peritext-website/images/glen.jpeg`,
-        description: [
-          "Glen Layne-Worthey is Associate Director of HathiTrust Research Center.",
-        ],
-        social: "https://ischool.illinois.edu/people/glen-layne-worthey",
-      },
-      {
-        name: "Amy Kirchhoff",
+  
+          social:
+            "https://www.colorado.edu/cmci/people/information-science/robin-burke",
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: "Advisory Board members",
+      members: [
+        {
+          name: "Stephen Downie",
+          image: "/peritext-website/images/stephen.jpeg",
+          description: [
+            "Dr. J. Stephen Downie is associate dean for research and a professor at the School of Information Sciences, and the Illinois codirector of the HathiTrust Research Center. He has been an active participant in the digital libraries and digital humanities research domains.",
+          ],
+          social: "https://ischool.illinois.edu/people/j-stephen-downie",
+        },
+        {
+          name: "Glen Layne-Worthey",
+          image: "/peritext-website/images/glen.jpeg",
+          description: [
+            "Glen Layne-Worthey is Associate Director of HathiTrust Research Center.",
+          ],
+          social: "https://ischool.illinois.edu/people/glen-layne-worthey",
+        },
+        {
+        name:"Amy Kirchhoff",
         image: "/peritext-website/images/amy.jpeg",
-        description: [
-          "Amy Kirchhoff is Senior Manager at Constellate (ITHAKA). ",
+        description:[
+        "Amy Kirchhoff is Senior Manager at Constellate (ITHAKA).",
         ],
         social: "https://www.linkedin.com/in/amy-kirchhoff/",
-      },
-      {
-<<<<<<< HEAD
-      name: "Peter Organisciak".
-      image: "/peritext-website/images/peter.jpeg",
-      description: [
-      "Dr. Peter Organisciak is an Associate Professor at the University of Denver who specializes in information science with the focus on artificial intelligence in education, large-scale text analysis and crowdsourcing."
-      ],
-      social: "https://morgridge.du.edu/about/faculty-directory/peter-organisciak",
-=======
+        },
+        {
         name: "Peter Organisciak",
         image: "/peritext-website/images/peter.jpeg",
         description: [
-          "Peter Organisciak is an Associate Professor at the University of Denver who specializes in information science with the focus on artificial intelligence in education, large-scale text analysis and crowdsourcing.",
+        "Dr. Peter Organisciak is an Associate Professor at the University of Denver who specializes in information science with the focus on artificial intelligence in education, large-scale text analysis and crowdsourcing.",
         ],
-        social:
-          "https://morgridge.du.edu/about/faculty-directory/peter-organisciak",
->>>>>>> 5faf88c2b545a3661a0ebc768b99db8bf1f5d657
-      },
-    ],
-  },
-  {
-    id: 3,
-    title: "Student Researchers",
-    members: [
-      {
-        name: "Tanmoy Debnath",
-        image: `/peritext-website/images/tanmoy.jpeg`,
-        description: [
-          "Tanmoy Debnath is a Master’s student in Information Management at the University of Illinois Urbana-Champaign (UIUC), specializing in data science and machine learning. His research focuses on natural language processing, drawing from his ongoing work as a Research Assistant at UIUC's Center for Children's Books.",
-        ],
-        social: "https://www.linkedin.com/in/tan-debnath/",
-      },
-      {
-        name: "Saksham Jain",
-        image: `/peritext-website/images/saksham.jpeg`,
-        description: [
-          "Saksham Jain holds a Master’s in Industrial Engineering with a focus on Advanced Analytics from the University of Illinois Urbana-Champaign. He joined ARI to apply his knowledge of natural language processing in a research setting, contributing to NLP solutions for the peritext-website Project. Saksham’s experience at ARI has deepened his expertise in large language models and practical applications of data science.",
-        ],
-        social: "https://www.linkedin.com/in/saksham-jain-uiuc",
-      },
-      {
-        name: "Mohney Raza",
-        image: `/peritext-website/images/mohney.jpeg`,
-        description: [
-          "Mohney Raza is pursuing a Bachelor’s in Information Studies with a minor in Computer Science at the University of Illinois Urbana-Champaign.",
-        ],
-        social: "https://www.linkedin.com/in/mohney-r-",
-      },
-    ],
-  },
-];
+        social: "https://morgridge.du.edu/about/faculty-directory/peter-organisciak",
+          
+        },
+      ],
+    },
+    {
+      id: 3,
+      title: "Student Researchers",
+      members: [
+        {
+          name: "Tanmoy Debnath",
+          image: "/peritext-website/images/tanmoy.jpeg",
+          description: [
+            "Tanmoy Debnath is a Master’s student in Information Management at the University of Illinois Urbana-Champaign (UIUC), specializing in data science and machine learning. His research focuses on natural language processing, drawing from his ongoing work as a Research Assistant at UIUC's Center for Children's Books.",
+          ],
+          social: "https://www.linkedin.com/in/tan-debnath/",
+        },
+        {
+          name: "Saksham Jain",
+          image: `/peritext-website/images/saksham.jpeg`,
+          description: [
+            "Saksham Jain holds a Master’s in Industrial Engineering with a focus on Advanced Analytics from the University of Illinois Urbana-Champaign. He joined ARI to apply his knowledge of natural language processing in a research setting, contributing to NLP solutions for the peritext-website Project. Saksham’s experience at ARI has deepened his expertise in large language models and practical applications of data science.",
+          ],
+          social: "https://www.linkedin.com/in/saksham-jain-uiuc",
+        },
+        {
+          name: "Mohney Raza",
+          image: `/peritext-website/images/mohney.jpeg`,
+          description: [
+            "Mohney Raza is pursuing a Bachelor’s in Information Studies with a minor in Computer Science at the University of Illinois Urbana-Champaign.",
+          ],
+          social: "https://www.linkedin.com/in/mohney-r-",
+        },
+      ],
+    },
+  ];
+  
